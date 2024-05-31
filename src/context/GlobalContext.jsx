a7b928c7cd5b3256896a42942b3df93603874d17
daw2-barrio23 <barriomunoznicolas@fpllefia.com>
2024-05-31 17:19:46 +0200

    se conecta a la api y funciona

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,6 +1,5 @@
 // GlobalContext.jsx
 import React, { createContext, useState, useEffect } from 'react';
-import bd from '../bd/bd.json';
 
 const GlobalContext = createContext();
 
@@ -11,7 +10,28 @@ const GlobalProvider = ({ children }) => {
   });
 
   useEffect(() => {
-    setDades(bd);
+    const fetchData = async () => {
+      try {
+        const responsePendientes = await fetch('https://json-server-examen-uf-4-eta.vercel.app/ticketsPendientes');
+        const responseResueltos = await fetch('https://json-server-examen-uf-4-eta.vercel.app/ticketsResueltos');
+
+        if (!responsePendientes.ok || !responseResueltos.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const ticketsPendientes = await responsePendientes.json();
+        const ticketsResueltos = await responseResueltos.json();
+
+        setDades({
+          ticketsPendientes,
+          ticketsResueltos
+        });
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
